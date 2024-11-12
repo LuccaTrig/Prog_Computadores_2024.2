802f3c21fae39415b3910c0a8ebf06f2688ab731
LuccaTrig <lhtrigueiro@gmail.com>
2024-11-12 20:31:45 -0300

    Update PythonLista-1.py

--- PythonLista-1.py
+++ PythonLista-1.py
@@ -2,17 +2,16 @@
 Faça um programa que aceita um número de até 4 dígitos (0 a 9999) e exiba a soma dos seus algarismos.
 '''
 
-#Dica // e %
-
 numero = int(input('Numero: '))
-numMilhar = numero // 1000
-restoCent = numero % 1000
-baseCent = numMilhar
-numCent = baseCent // 100
-restoDecim = baseCent % 100
-baseDecim = restoDecim
-numDecim = restoDecim // 10
-restoUnid = baseDecim % 10
-Unidade = restoUnid
 
-print(f'Numero: {numero}. Milhar:{numMilhar}; Centena: {numCent}; decimal: {numDecim}; Unidade: {Unidade} ')
+if: numero < -999
+
+milhar = numero // 1000
+numeroCent = numero % 1000
+centena = numeroCent // 100
+opDecimal = numeroCent % 100
+dezena = opDecimal // 10
+unidade = opDecimal % 10 
+somaAlgarismos = milhar + centena + dezena + unidade
+
+print(f' Numero = {numero} milhar = {milhar} centena = {centena} Dezena = {dezena} unidade = {unidade} soma dos algarismos = {somaAlgarismos}')
